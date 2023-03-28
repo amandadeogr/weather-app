fcf867cdbdaaefd7de33548d64eed48175addebf
amandadeog <amandadeog@gmail.com>
2023-03-28 16:33:50 -0300

    declare variables

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const apiKey = "6d07a2e569fd15380570cc8e1e58a515";
+
+const inputCity = document.querySelector('#input-city');
+const btnSearch = document.querySelector('#input-search');
+
+const cityElement = document.querySelector('#location');
+const tempElement = document.querySelector('#temperature');
+const dataTimeElement = document.querySelector('#date-time');
+const weatherImageElement = document.querySelector('#weather-image');
+const umidityElement = document.querySelector('#umidity');
+const windElement = document.querySelector('#wind');
