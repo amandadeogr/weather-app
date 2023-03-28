c64c82b80f749ca84791fa5e0cf199d444b93763
amandadeog <amandadeog@gmail.com>
2023-03-28 17:06:48 -0300

    create getWeatherData function

--- js/script.js
+++ js/script.js
@@ -4,15 +4,26 @@ const inputCity = document.querySelector('#input-city');
 const btnSearch = document.querySelector('#input-search');
 
 const cityElement = document.querySelector('#location');
-const tempElement = document.querySelector('#temperature');
+const tempElement = document.querySelector('#temp');
 const dataTimeElement = document.querySelector('#date-time');
 const weatherImageElement = document.querySelector('#weather-image');
 const umidityElement = document.querySelector('#umidity');
 const windElement = document.querySelector('#wind');
 
 // functions
-const showWeatherData = (city) => {
-   console.log(city);
+const getWeatherData = async(city) => {
+   const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt-br`;
+
+   const res = await fetch(apiWeatherURL);
+   const data = await res.json();
+
+   console.log(data)
+   return data;
+}
+
+
+const showWeatherData = async (city) => {
+   const data = await getWeatherData(city);
 }
 
 //events
