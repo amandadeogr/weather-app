1b68bd06951ae2d26fef35de0436249946700f19
amandadeog <amandadeog@gmail.com>
2023-03-28 21:20:02 -0300

    create getWeatherCondition function

--- js/script.js
+++ js/script.js
@@ -22,6 +22,34 @@ const getWeatherData = async(city) => {
    return data;
 }
 
+const getWeatherCondition = async (city) => {
+   const data = await getWeatherData(city);
+   const dataWeather =  data.weather[0].main;
+
+   console.log(dataWeather)
+
+   if(dataWeather == 'Clouds') {
+      weatherImageElement.setAttribute("src", "./assets/images/clouds.png");
+      document.body.style.backgroundImage = "url('./assets/images/cloudy.jpg')";
+
+   } else if (dataWeather == 'Rain') {
+      weatherImageElement.setAttribute("src", "./assets/images/rain.png");
+      document.body.style.backgroundImage = "url('./assets/images/rain.jpg')";
+
+   } else if (dataWeather == 'Clear') {
+      weatherImageElement.setAttribute("src", "./assets/images/moon.png");
+      document.body.style.backgroundImage = "url('./assets/images/clear.jpg')";
+
+   } else if (dataWeather == 'Snow') {
+      weatherImageElement.setAttribute("src", "./assets/images/snow.png");
+      document.body.style.backgroundImage = "url('./assets/images/snow.jpg')";
+
+   }else if (dataWeather == 'haze') {
+      weatherImageElement.setAttribute("src", "./assets/images/haze.png");
+      document.body.style.backgroundImage = "url('./assets/images/cloudy.jpg')";
+   }
+}
+
 const showWeatherData = async (city) => {
    const data = await getWeatherData(city);
 
@@ -32,6 +60,7 @@ const showWeatherData = async (city) => {
    humidityElement.innerText = data.main.humidity;
    windElement.innerText = data.wind.speed;
 
+   getWeatherCondition(city)
 }
 
 //events
